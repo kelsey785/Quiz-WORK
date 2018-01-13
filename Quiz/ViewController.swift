9c598bee306431ba213cfb2709ce6e305d3e21fb
Kelsey <kquinn1@highpoint.edu>
2018-01-13 15:31:13 -0500

    workkk

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -2,24 +2,55 @@
 //  ViewController.swift
 //  Quiz
 //
-//  Created by Tara Kelley on 1/12/18.
+//  Created by Kelsey on 1/12/18.
 //  Copyright © 2018 Kelsey Quinn. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+    "What is 7+7",
+    "What is the capital of Vermont?",
+    "What is cognac made from"
+    ]
+    let answers: [String] = [
+    "14",
+    "Montpelier",
+    "Grapes"
+    ]
+    var currentQuestionsIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton)
+    {
+        currentQuestionsIndex += 1
+        if currentQuestionsIndex == questions.count
+        {
+            currentQuestionsIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionsIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showAnswer(_ sender: UIButton)
+    {
+        let answer: String = answers[currentQuestionsIndex]
+        answerLabel.text = answer
+        
     }
-
-
+    
+        override func viewDidLoad()
+        {
+            super.viewDidLoad()
+            questionLabel.text = questions[currentQuestionsIndex]
+        }
+    
 }
 
